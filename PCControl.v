a90a2af24e91dff6e55552d4730e23c003e2387d
Alex Martin <gradient.am@gmail.com>
2017-05-22 19:03:35 -0700

    PCControl

---
+++ PCControl.v
@@ -0,0 +1,20 @@
+/*
+    in_ctrl_btype:
+        0: BRZ - branch if zero
+        1: BRN - branch if negative
+*/
+module PCControl (
+        input in_ctrl_branch,
+        input in_ctrl_btype,
+        input in_ctrl_jump,
+        input in_ctrl_neg,
+        input in_ctrl_zero,
+        output reg out
+    );
+
+    always @(*)
+        out = in_ctrl_jump ||
+            (in_ctrl_branch && !in_ctrl_btype && !in_ctrl_neg && in_ctrl_zero) ||
+            (in_ctrl_branch && in_ctrl_btype && in_ctrl_neg);
+
+endmodule // PCControl
