8bfa145498c741a19124d93e81832fcb3d1028ce
Kush Mahajani <kmahajani200@gmail.com>
2017-05-22 18:53:00 -0700

    ID/EX Buffer

--- IF-IDBuffer.v
+++ IF-IDBuffer.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: Kush Mahajani
-// 
-// Create Date: 05/22/2017 05:59:55 PM
-// Design Name: 
-// Module Name: ifidbuffer.v
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module ifidbuffer(
     input clk,
