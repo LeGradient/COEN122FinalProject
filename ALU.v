1977697c9849a970c8d7556ec6b33c0f7b4d804a
Alex Martin <gradient.am@gmail.com>
2017-05-25 16:13:01 -0700

    ALU

---
+++ ALU.v
@@ -0,0 +1,22 @@
+module ALU (
+        input clk,
+        input [31:0] in_a,
+        input [31:0] in_b,
+        input [2:0] ctrl_aluop,
+        output reg [31:0] out
+    );
+
+    always @(posedge clk) begin
+        if (ctrl_aluop == 'b100)
+            out = in_b + in_a;
+        else if (ctrl_aluop == 'b010)
+            out = in_a + 1;
+        else if (ctrl_aluop == 'b001)
+            out = -in_a;
+        else if (ctrl_aluop == 'b000)
+            out = in_b - in_a;
+        else if (ctrl_aluop == 'b111)
+            out = in_a;
+    end
+
+endmodule // ALU
