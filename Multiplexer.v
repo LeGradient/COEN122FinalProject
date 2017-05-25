a107654bd2b500adf5dbcac29b082bf5ed5cdc45
Kush Mahajani <kmahajani200@gmail.com>
2017-05-25 16:35:42 -0700

    Add multiplexer

---
+++ Multiplexer.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+
+module mux(
+    input in_data0,
+    input in_data1,
+    input select,
+    output reg out_data
+    );
+    
+    always @ (in_data0 or in_data1 or select)
+    begin
+        case (select)
+            2'b00 : out_data = in_data0;
+            2'b01 : out_data = in_data1;
+        endcase
+     end
+endmodule
