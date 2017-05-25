d3cf0c1f1fef122a12bd37939c45841f34f68e74
Kush Mahajani <kmahajani200@gmail.com>
2017-05-25 16:44:45 -0700

    DataMemory.v

---
+++ DataMemory.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+
+module datamem(
+    input clk,
+    input read,
+    input write,
+    input [31:0] addr,
+    input [31:0] in,
+    output reg [31:0] out
+    );
+    
+    reg [31:0] mem [65535:0];
+    
+    integer i;
+    initial begin
+        for (i = 0; i < 65536; i = i + 1) begin
+            mem[i] = i;
+        end
+    end
+    
+    always @(posedge clk) begin
+        if (write == 1)
+            mem[addr] = in;
+        if (read == 1)
+            out = mem[addr];
+    end
+endmodule
