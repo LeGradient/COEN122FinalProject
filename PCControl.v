9a7dfad4e57b45faebf50e18fa1454f5e0d091e3
Alex Martin <gradient.am@gmail.com>
2017-06-02 15:00:56 -0700

    Optimized PCControl

--- PCControl.v
+++ PCControl.v
@@ -15,8 +15,8 @@ module PCControl (
     );
 
     always @(in_ctrl_branch or in_ctrl_btype or in_ctrl_jump or in_ctrl_neg or in_ctrl_zero)
-        out = in_ctrl_jump ||
-            (in_ctrl_branch && !in_ctrl_btype && !in_ctrl_neg && in_ctrl_zero) ||
-            (in_ctrl_branch && in_ctrl_btype && in_ctrl_neg);
+        out = in_ctrl_jump
+            || (in_ctrl_branch && !in_ctrl_btype && in_ctrl_zero)
+            || (in_ctrl_branch && in_ctrl_btype && in_ctrl_neg);
 
 endmodule // PCControl
