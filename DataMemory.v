3ab2201444ae6aa28d526a716699c789964bc02f
Kush Mahajani <kmahajani200@gmail.com>
2017-06-02 14:14:51 -0700

    More consistent variable names

--- DataMemory.v
+++ DataMemory.v
@@ -2,11 +2,11 @@
 
 module DataMemory(
     input clk,
-    input read,
-    input write,
-    input [31:0] addr,
-    input [31:0] in,
-    output reg [31:0] out
+    input in_ctrl_read,
+    input in_ctrl_write,
+    input [31:0] in_addr,
+    input [31:0] in_data,
+    output reg [31:0] out_data
     );
     
     reg [31:0] mem [65535:0];
@@ -19,9 +19,9 @@ module DataMemory(
     end
     
     always @(posedge clk) begin
-        if (write == 1)
-            mem[addr] = in;
-        if (read == 1)
-            out = mem[addr];
+        if (in_ctrl_write == 1)
+            mem[in_addr] = in_data;
+        if (in_ctrl_read == 1)
+            out_data = mem[in_addr];
     end
 endmodule
