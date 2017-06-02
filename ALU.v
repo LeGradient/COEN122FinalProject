eb4b1b5044b709eabd256cb32914db6a63e66196
Alex Martin <gradient.am@gmail.com>
2017-06-02 15:48:51 -0700

    Fixed ALU to not change flags on a NOP

--- ALU.v
+++ ALU.v
@@ -1,6 +1,6 @@
-/* FIXME: 
+/* FIXME:
 FIX PASS AND NOOP
-*/ 
+*/
 `timescale 1ns / 1ps
 
 module ALU (
@@ -14,19 +14,22 @@ module ALU (
     );
 
     always @(posedge clk) begin
-        if (in_ctrl_aluop == 'b100)
+        if (in_ctrl_aluop == 3'b100)
             out_result = in_b + in_a;
-        else if (in_ctrl_aluop == 'b010)
+        else if (in_ctrl_aluop == 3'b010)
             out_result = in_a + 1;
-        else if (in_ctrl_aluop == 'b001)
+        else if (in_ctrl_aluop == 3'b001)
             out_result = -in_a;
-        else if (in_ctrl_aluop == 'b000)
+        else if (in_ctrl_aluop == 3'b000)
             out_result = in_b - in_a;
-        else if (in_ctrl_aluop == 'b111)
+        else if (in_ctrl_aluop == 3'b111)
             out_result = in_a;
 
-        out_zero = out_result == 0;
-        out_neg = out_result < 0;
+        // don't change flags if NOP
+        if (in_ctrl_aluop != 3'b011) begin
+            out_zero = out_result == 0;
+            out_neg = out_result < 0;
+        end
     end
 
 endmodule // ALU
