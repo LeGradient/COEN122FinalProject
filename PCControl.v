fe85079a06a180a6165c86870ab5855ca9c03a78
Alex Martin <gradient.am@gmail.com>
2017-06-04 15:15:19 -0700

    PCControl initializes to 0

--- PCControl.v
+++ PCControl.v
@@ -14,6 +14,9 @@ module PCControl (
         output reg out
     );
 
+    initial
+        out = 0;
+
     always @(in_ctrl_branch or in_ctrl_btype or in_ctrl_jump or in_ctrl_neg or in_ctrl_zero)
         out = in_ctrl_jump
             || (in_ctrl_branch && !in_ctrl_btype && in_ctrl_zero)
