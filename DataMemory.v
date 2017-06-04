a76d4a08bc695d6c76eab0f1a07e3bc5d168cb92
Alex Martin <gradient.am@gmail.com>
2017-06-04 15:17:05 -0700

    Fixed bounds for memory loading

--- DataMemory.v
+++ DataMemory.v
@@ -8,9 +8,9 @@ module DataMemory(
     input [31:0] in_data,
     output reg [31:0] out_data
     );
-    
+
     reg [31:0] mem [65535:0];
-    
+
     integer i;
     initial begin
         for (i = 0; i < 65536; i = i + 1) begin
@@ -18,18 +18,18 @@ module DataMemory(
         end
 
         // DATA 1
-        $readmemh("data1.txt", mem, 8'h100, 8'h107);
+        $readmemh("data1.txt", mem, 'h100, 'h107);
 
         // DATA 2
-        $readmemh("data2.txt", mem, 8'h80, 8'h89);
+        $readmemh("data2.txt", mem, 'h80, 'h89);
 
         // DATA 3
-        $readmemh("data3.txt", mem, 8'h40, 8'h45);
+        $readmemh("data3.txt", mem, 'h40, 'h45);
 
         // DATA 4
-        $readmemh("data4.txt", mem, 8'h20, 8'h31);
+        $readmemh("data4.txt", mem, 'h20, 'h2B);
     end
-    
+
     always @(posedge clk) begin
         if (in_ctrl_write == 1)
             mem[in_addr] = in_data;
