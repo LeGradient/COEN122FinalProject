837dcd8e5840ff9672379f4d7183a34ebcccc455
Alex Martin <gradient.am@gmail.com>
2017-06-06 15:54:06 -0700

    Control: fixed lots of things

--- Control.v
+++ Control.v
@@ -29,36 +29,41 @@ module Control (
         out_ctrl_memrd = !in_opcode[3] && !in_opcode[2] && !in_opcode[1] && in_opcode[0]
                     || in_opcode[3] && in_opcode[1] && !in_opcode[0];
 
-        out_ctrl_memwrt = !in_opcode[3] && !in_opcode[2] && in_opcode[1];
+        out_ctrl_memwrt = in_opcode == 4'b0011;
 
-        out_ctrl_alusrc = in_opcode[3] && in_opcode[2];
+        out_ctrl_alusrc = in_opcode == 4'b1111;
 
         out_ctrl_jump = in_opcode[3] && !in_opcode[2] && !in_opcode[0];
 
         out_ctrl_regwrt = in_opcode[2]
                     || !in_opcode[3] && !in_opcode[1] && in_opcode[0];
 
-        out_ctrl_memtoreg = in_opcode[3] && !in_opcode[0];
+        out_ctrl_memtoreg = in_opcode == 4'b1110;
 
-        // ALUOp
-        // add
-        if (in_opcode == 4'b1111 || in_opcode == 4'b0100)
-            out_ctrl_aluop = 3'b100;
-        // increment
-        else if (in_opcode == 4'b0101)
-            out_ctrl_aluop = 3'b010;
-        // negate
-        else if (in_opcode == 4'b0110)
-            out_ctrl_aluop = 3'b001;
-        // subtract
-        else if (in_opcode == 4'b0111)
-            out_ctrl_aluop = 3'b000;
-        // pass
-        if (in_opcode == 4'b1000 || in_opcode == 4'b1001 || in_opcode == 4'b1011)
-            out_ctrl_aluop = 3'b111;
-        // nop
-        else
-            out_ctrl_aluop = 3'b011;
+        out_ctrl_branch = in_opcode == 4'b1001 || in_opcode == 4'b1011;
+
+        out_ctrl_btype = in_opcode == 4'b1011;
+
+        // ALUop
+        begin
+            case (in_opcode)
+            // add
+                4'b1111: out_ctrl_aluop = 3'b100;
+                4'b0100: out_ctrl_aluop = 3'b100;
+            // increment
+                4'b0101: out_ctrl_aluop = 3'b010;
+            // negate
+                4'b0110: out_ctrl_aluop = 3'b001;
+            // subtract
+                4'b0111: out_ctrl_aluop = 3'b000;
+            // pass
+                4'b1000: out_ctrl_aluop = 3'b111;
+                4'b1001: out_ctrl_aluop = 3'b111;
+                4'b1011: out_ctrl_aluop = 3'b111;
+            // nop
+                default: out_ctrl_aluop = 3'b011;
+            endcase
+        end
     end
 
 endmodule // Control
