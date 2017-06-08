55bc089aeba93268c4e6c1821325cff7737b67fe
Alex Martin <gradient.am@gmail.com>
2017-06-08 14:26:35 -0700

    ITS ALIIIIVE

--- RegisterFile.v
+++ RegisterFile.v
@@ -19,21 +19,21 @@ module RegisterFile (
             regfile[i] = 0;
         end
 
-        // DATA 1
-        regfile[1] = 8;
-        regfile[2] = 'h100;
+        // // DATA 1
+        // regfile[1] = 8;
+        // regfile[2] = 'h100;
 
-        /*// DATA 2
-        regfile[1] = 10;
-        regfile[2] = 'h80;
+        // // DATA 2
+        // regfile[1] = 10;
+        // regfile[2] = 'h80;
 
-        // DATA 3
-        regfile[1] = 6;
-        regfile[2] = 'h40;
+        // // DATA 3
+        // regfile[1] = 6;
+        // regfile[2] = 'h40;
 
         // DATA 4
         regfile[1] = 12;
-        regfile[2] = 'h20;*/
+        regfile[2] = 'h20;
 
         out_rsval = 0;
         out_rtval = 0;
@@ -45,5 +45,8 @@ module RegisterFile (
         out_rsval = regfile[in_rs];
         out_rtval = regfile[in_rt];
     end
+
+    always @(regfile[3])
+        $display("Regfile[3]: %h", regfile[3]);
 
 endmodule // RegisterFile
